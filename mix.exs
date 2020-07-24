a2d6e77dbd571e5ceee6f4907b95861bc77d24dc
Joel C <joel@vorce.se>
2020-07-24 09:46:55 +0200

    Upgrade to elixir 1.10, fix #4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Hoeg.MixProject do
     [
       app: :hoeg,
       version: "0.2.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.10",
       elixirc_options: [warnings_as_errors: true],
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -14,20 +14,16 @@ defmodule Hoeg.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:nimble_parsec, "~> 0.6"},
       {:excoveralls, "~> 0.8", only: :test}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
 end
